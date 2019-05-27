ccf1771f9fda777efd39ef230c6cb8c4a583c065
jennifer mashaal <jennifermashaal@MacBook-Air-6.local>
2019-05-27 12:38:34 -0400

    added resume

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -25,3 +25,23 @@ describe('HomePage', () => {
     expect(component).toBeTruthy();
   });
 });
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  public myDateOfBirth: String;
+
+  ionViewDidEnter() {
+    this.myDateOfBirth = "1994-10-03";
+  }
+
+  pressMe() {
+    alert("Hi!!");
+  }
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,65 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular'
+import { ToastController } from '@ionic/angular'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+@Component({
+  selector: 'toast-example',
+  templateUrl: 'home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+
 export class HomePage {
 
-  constructor() {}
 
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Warning!',
+      subHeader: 'You rock',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+   alert.present();
+  }
+
+  async presentToastWithOptions() {
+    const toast = await this.toastController.create({
+      header: 'Congratulations',
+      message: 'We made a toast',
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'beer',
+          text: 'Cheers!',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }   
+              ] 
+        }
+    );
+    toast.present();
+  }
 }
+
+
+  
+  
+
+
+
